5d0d1a2224bff6f447ac426beb3df3d961caa58f
versus <versus.dev@gmail.com>
2020-11-28 15:07:58 +0200

    Brew formula update for terraform-switcher version 0.11.2

--- Formula/tfswitch.rb
+++ Formula/tfswitch.rb
@@ -2,16 +2,17 @@
 class Tfswitch < Formula
   desc "The tfswitch command lets you switch between terraform versions."
   homepage "https://versus.github.io/terraform-switcher"
-  version "0.11.1"
+  version "0.11.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/versus/terraform-switcher/releases/download/0.11.1/terraform-switcher_0.11.1_darwin_amd64.tar.gz"
-    sha256 "06a874b4ac46a0c36e07e1b19abb62cdb22acb91ef6ebf1d36800a7632c6ec21"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/versus/terraform-switcher/releases/download/0.11.1/terraform-switcher_0.11.1_linux_amd64.tar.gz"
-    sha256 "9732f6b30fdab7b5872cb970c1094568b5e28ffc8aaca9b1dcbc5d61c52c58f7"
+    url "https://github.com/versus/terraform-switcher/releases/download/0.11.2/terraform-switcher_0.11.2_darwin_amd64.tar.gz"
+    sha256 "e66a5daa595774899a698f89143e9bda243c1a74b180bbe455ff641bb9328f76"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/versus/terraform-switcher/releases/download/0.11.2/terraform-switcher_0.11.2_linux_amd64.tar.gz"
+      sha256 "e5c4b08b61f88c08dea57c616cb13baa7ace66bc168b85604f45c23d7970608d"
+    end
   end
   
   conflicts_with "terraform"
