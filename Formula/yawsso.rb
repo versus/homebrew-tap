0883c6cc2b7a653487a699f8bc0dbed0909d0940
Valentyn <versus.dev@gmail.com>
2025-06-23 16:01:41 +0200

    fix sha sum

--- Formula/yawsso.rb
+++ Formula/yawsso.rb
@@ -6,6 +6,8 @@ class Yawsso < Formula
   url "https://github.com/victorskl/yawsso/archive/refs/tags/1.2.1.tar.gz"
   sha256 "db5f77cb57d8b164b591b94d395e2fa2ccd45b68c80c04d189ace8799a928064"
 
+
+  
   depends_on "python"
 
   def install
